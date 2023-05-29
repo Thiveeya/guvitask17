14d2459dda098fcb7288b7e3c19dc1b87d26404d
Thiveeya <130544175+Thiveeya@users.noreply.github.com>
2023-05-29 11:11:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+
+    var txt = document.getElementById('text');
+    var msg = document.getElementById('msg');
+    msg.style.visibility = 'hidden';
+
+    txt.innerHTML = 10;
+    setTimeout(function () {
+        txt.innerHTML = 9;
+        setTimeout(function () {
+            txt.innerHTML = 8;
+            setTimeout(function () {
+                txt.innerHTML = 7;
+                setTimeout(function () {
+                    txt.innerHTML = 6;
+                    setTimeout(function () {
+                        txt.innerHTML = 5;
+                        setTimeout(function () {
+                            txt.innerHTML = 4;
+                            setTimeout(function () {
+                                txt.innerHTML = 3;
+                                setTimeout(function () {
+                                    txt.innerHTML = 2;
+                                    setTimeout(function () {
+                                        txt.innerHTML = 1;
+                                        setTimeout(function () {
+                                            msg.style.visibility = 'visible';
+                                            txt.style.visibility = 'hidden';
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+
+}
